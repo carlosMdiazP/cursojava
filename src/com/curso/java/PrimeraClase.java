f834aeb94813b9c25538b0b9ea76b8811f6b67c9
Tardes <Tardes@SB11-15>
2024-09-19 19:44:41 +0200

    Cambios primera clase

--- src/com/curso/java/PrimeraClase.java
+++ src/com/curso/java/PrimeraClase.java
@@ -7,7 +7,9 @@ public class PrimeraClase {
 		System.out.println(nombre);
 		String apellido = "Diaz";
 		String apellido2 = "Peña";
+		String curso="1A";
 		System.out.println(apellido);
 		System.out.println(apellido2);
+		System.out.println(curso);
 	}
 }
