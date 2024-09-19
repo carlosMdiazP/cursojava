1865954d47e64a48b7522018014ce0475c9678f5
carlosMdiazP <carlosmanueldiazp@gmail.com>
2024-09-19 19:57:13 +0200

    Update HolaMundo.java desde Github

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo2");
 		String nombre = "carlos";		
 		System.out.println(nombre );		
 		String apellido = "Diaz";		
